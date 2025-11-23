12e3973fc77935e40396a642d571ed95461b5e7d
Hoang Van Duong <hd2005555@gmail.com>
2025-11-23 20:52:57 +0700

    Add update method in FileService

--- src/main/java/com/laptrinhmang/service/FileService.java
+++ src/main/java/com/laptrinhmang/service/FileService.java
@@ -8,4 +8,7 @@ public class FileService {
     public boolean updateStatus(FileEntity fileEntity, Status status) {
         return new FileDAO().updateStatusFile(status, fileEntity.getId());
     }
+    public boolean update(FileEntity fileEntity) {
+        return new FileDAO().updateFile(fileEntity);
+    }
 }

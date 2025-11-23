f276d479999a4bac19d341fd54bf8190cf418100
Hoang Van Duong <hd2005555@gmail.com>
2025-11-23 12:26:00 +0700

    Implement task execution with file processing and status update

--- src/main/java/com/laptrinhmang/threadpool/TaskExecutor.java
+++ src/main/java/com/laptrinhmang/threadpool/TaskExecutor.java
@@ -1,4 +1,45 @@
 package com.laptrinhmang.threadpool;
 
-public class TaskExecutor {
+import com.google.gson.Gson;
+import com.laptrinhmang.App;
+import com.laptrinhmang.bean.FileEntity;
+import com.laptrinhmang.service.ConvertService;
+import com.laptrinhmang.service.FileService;
+import com.laptrinhmang.service.MinioService;
+import com.laptrinhmang.service.RedisService;
+import com.laptrinhmang.bean.Status;
+import java.io.File;
+
+public class TaskExecutor implements Runnable {
+    private FileEntity fileEntity;
+    private RedisService redisService;
+    private MinioService minioService;
+    private ConvertService convertService;
+    private FileService fileService;
+    public TaskExecutor(FileEntity fileEntity) {
+        this.fileEntity = fileEntity;
+        this.redisService = new RedisService();
+        this.minioService = new MinioService();
+        this.convertService = new ConvertService();
+        this.fileService = new FileService();
+    }
+
+    @Override
+    public void run() {
+        try{
+            File pdfFile = minioService.download(fileEntity.getLink_pdf());
+            fileService.updateStatus(fileEntity, Status.PROCESSING);
+            redisService.cacheSet(String.valueOf(fileEntity.getId()), fileEntity);
+            File resultFile = new File(convertService.convertFullProcess(pdfFile,"A:/temp/" + fileEntity.getId()+"/"+fileEntity.getLink_pdf()));
+            minioService.upload(resultFile);
+
+            fileService.updateStatus(fileEntity, Status.SUCCESS);
+
+            resultFile.delete();
+            System.out.println("File " + fileEntity.getId() + " converted and uploaded to Minio");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

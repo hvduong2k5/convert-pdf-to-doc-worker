f52b8f852c167570b874be3fcbd79eade6946733
Hoang Van Duong <hd2005555@gmail.com>
2025-11-23 12:24:28 +0700

    Add FileService with updateStatus method

---
+++ src/main/java/com/laptrinhmang/service/FileService.java
@@ -0,0 +1,11 @@
+package com.laptrinhmang.service;
+
+import com.laptrinhmang.bean.FileEntity;
+import com.laptrinhmang.bean.Status;
+import com.laptrinhmang.dao.FileDAO;
+
+public class FileService {
+    public boolean updateStatus(FileEntity fileEntity, Status status) {
+        return new FileDAO().updateStatusFile(status, fileEntity.getId());
+    }
+}
